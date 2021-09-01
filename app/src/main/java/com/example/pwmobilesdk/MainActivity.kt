a0eaac05c8c5ca3b480fe76a8e64ed00a573c58d
Christopher Stock <christopher.stock@mayflower.de>
2021-09-01 14:40:38 +0200

    Add modal on selecting settings item.

--- app/src/main/java/com/example/pwmobilesdk/MainActivity.kt
+++ app/src/main/java/com/example/pwmobilesdk/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.pwmobilesdk
 
+import android.app.AlertDialog
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +11,30 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
 import com.example.pwmobilesdk.databinding.ActivityMainBinding
+import android.widget.Toast
+import io.mpos.transactions.receipts.Receipt
+import io.mpos.transactionprovider.TransactionProcessDetailsState
+import io.mpos.transactionprovider.TransactionProcessDetails
+import io.mpos.transactionprovider.TransactionProcess
+import io.mpos.paymentdetails.DccInformation
+import io.mpos.paymentdetails.ApplicationInformation
+import io.mpos.android.shared.BitmapHelper
+import android.graphics.Bitmap
+import android.util.Log
+import io.mpos.transactionprovider.TransactionProcessWithRegistrationListener
+import io.mpos.transactions.parameters.TransactionParameters
+import io.mpos.accessories.AccessoryFamily
+import io.mpos.accessories.parameters.AccessoryParameters
+import io.mpos.provider.ProviderMode
+import io.mpos.Mpos.createTransactionProvider
+import io.mpos.transactions.Currency
+import io.mpos.transactions.Transaction
+import java.math.BigDecimal
+import java.util.*
+import android.content.DialogInterface
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -41,6 +66,34 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        Log.d( "payworks", "onOptionsItemSelected" )
+
+        if ( item.itemId == R.id.action_settings )
+        {
+            Log.d( "payworks", "actionSettings" )
+
+            Toast.makeText( this, "Let's do a test transaction ..", Toast.LENGTH_LONG ).show()
+
+            Log.d( "payworks", "Toast - shown!" )
+
+            val builder = AlertDialog.Builder( this )
+/*
+            val items = mutableListOf( "1", "2", "3" );
+*/
+            builder.setTitle("Dialog Title")
+            builder.setMessage("Dialog Text Dialog Text Dialog Text Dialog Text Dialog Text Dialog Text Dialog Text")
+/*
+                .setItems(
+                    items,
+                    DialogInterface.OnClickListener {
+                        dialog, which ->
+                        Log.d( "payworks", "Klick on Dialog" )
+                    }
+*/
+            builder.create().show()
+        }
+
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -54,5 +107,132 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
+    }
+
+    fun transaction() {
+        val transactionProvider = createTransactionProvider(
+            this, ProviderMode.TEST,
+            "MERCHANT_IDENTIFIER",
+            "MERCHANT_SECRET_KEY"
+        )
+
+        /* For starting transaction in mocked mode use fallowing provider:
+    final TransactionProvider transactionProvider = Mpos.createTransactionProvider(this,
+            ProviderMode.MOCK,
+            "merchantIdentifier",
+            "merchantSecretKey");
+    */
+
+
+        /* When using the Bluetooth Miura, use the following parameters: */
+        val accessoryParameters = AccessoryParameters.Builder(AccessoryFamily.MIURA_MPI)
+            .bluetooth()
+            .build()
+
+
+        /* When using Verifone readers via WiFi or Ethernet, use the following parameters:
+    AccessoryParameters accessoryParameters = new AccessoryParameters.Builder(AccessoryFamily.VERIFONE_VIPA)
+                                                                     .tcp("192.168.254.123", 16107)
+                                                                     .build();
+    */
+        val transactionParameters = TransactionParameters.Builder()
+            .charge(BigDecimal("5.00"), Currency.EUR)
+            .subject("Bouquet of Flowers")
+            .customIdentifier("yourReferenceForTheTransaction")
+            .build()
+        val paymentProcess = transactionProvider.startTransaction(transactionParameters, accessoryParameters,
+            object : TransactionProcessWithRegistrationListener {
+                override fun onRegistered(
+                    process: TransactionProcess?,
+                    transaction: Transaction
+                ) {
+                    Log.d(
+                        "mpos",
+                        "transaction identifier is: " + transaction.getIdentifier()
+                            .toString() + ". Store it in your backend so that you can always query its status."
+                    )
+                }
+
+                override fun onStatusChanged(
+                    process: TransactionProcess?,
+                    transaction: Transaction?,
+                    processDetails: TransactionProcessDetails
+                ) {
+                    Log.d("mpos", "status changed: " + Arrays.toString(processDetails.information))
+                }
+
+                override fun onCustomerSignatureRequired(
+                    process: TransactionProcess,
+                    transaction: Transaction?
+                ) {
+                    // in a live app, this image comes from your signature screen
+                    val conf = Bitmap.Config.ARGB_8888
+                    val bm = Bitmap.createBitmap(1, 1, conf)
+                    val signature = BitmapHelper.byteArrayFromBitmap(bm)
+                    process.continueWithCustomerSignature(signature, true)
+                }
+
+                override fun onCustomerVerificationRequired(
+                    process: TransactionProcess,
+                    transaction: Transaction?
+                ) {
+                    // always return false here
+                    process.continueWithCustomerIdentityVerified(false)
+                }
+
+                override fun onApplicationSelectionRequired(
+                    process: TransactionProcess,
+                    transaction: Transaction?,
+                    applicationInformation: List<ApplicationInformation?>
+                ) {
+                    // This happens only for readers that don't support application selection on their screen
+                    process.continueWithSelectedApplication(applicationInformation[0])
+                }
+
+                override fun onDccSelectionRequired(
+                    transactionProcess: TransactionProcess,
+                    transaction: Transaction?,
+                    dccInformation: DccInformation?
+                ) {
+
+                    // This comes up if the DCC selection cannot be done on the terminal itself
+                    transactionProcess.continueDccSelectionWithOriginalAmount()
+                }
+
+                override fun onCompleted(
+                    process: TransactionProcess?,
+                    transaction: Transaction,
+                    processDetails: TransactionProcessDetails
+                ) {
+                    Log.d("mpos", "completed")
+                    if (processDetails.state == TransactionProcessDetailsState.APPROVED) {
+                        // print the merchant receipt
+                        val merchantReceipt: Receipt = transaction.getMerchantReceipt()
+
+                        // print a signature line if required
+                        if (merchantReceipt.isSignatureLineRequired()) {
+                            println("")
+                            println("")
+                            println("")
+                            println("------ PLEASE SIGN HERE ------")
+                        }
+
+                        // ask the merchant, whether the shopper wants to have a receipt
+                        val customerReceipt: Receipt = transaction.getCustomerReceipt()
+
+                        // and close the checkout UI
+                    } else {
+                        // Allow your merchant to try another transaction
+                    }
+                }
+            })
+    }
+
+    override fun onBackPressed() {
+        Toast.makeText(
+            this,
+            "The back button is disabled during a transaction. Please use the 'abort' button to cancel the transaction.",
+            Toast.LENGTH_LONG
+        ).show()
     }
 }
